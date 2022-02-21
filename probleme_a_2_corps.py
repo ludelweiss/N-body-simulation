ded0f4ea552e5917cf768fe9bd92dd0caff2bf38
Lud <98480560+ludelweiss@users.noreply.github.com>
2022-02-21 20:02:43 +0100

    Simple relecture + rajout de commentaires sur ton code

--- probleme_a_2_corps.py
+++ probleme_a_2_corps.py
@@ -14,10 +14,11 @@ N=2
 i=3
 
 X=np.zeros((i,N))
-X[0,1]=1
+X[0,1]=1    #position initial de la particule B
 
 V=np.zeros((i,N))
-V[0,1]=1
+V[0,1]=1    #vitesse initiale de la particule B
+
 
 '''
 Norme des vitesses :
@@ -46,40 +47,40 @@ dt=0.001
 # Calcul du dv ajouté à chaque dt pour A :
 def fA(V, t):
     dV=np.zeros(3)
-    dV[0]=Fab(rab(X))*t
-    dV[1]=0
-    dV[2]=0
+    dV[0]= Fab(rab(X))*t
+    dV[1]= 0
+    dV[2]= 0
     return dV
 
 
 # Calcul du dv ajouté à chaque dt pour B :
 def fB(V, t):
     dV=np.zeros(3)
-    dV[0]=-Fab(rab(X))*t
-    dV[1]=0
-    dV[2]=0
+    dV[0]= -Fab(rab(X))*t
+    dV[1]= 0
+    dV[2]= 0
     return dV
-
-
-N=2
-i=3
 
 
 
 def euler(dt, X, V, N, i):
     tab_euler_X = np.zeros(i*N)
+    
     #calcul du tableau des X pour A et B :
     for n in range(0,N):
         for i in range(0,i) :
             tab_euler_X[i]=X[i,n]+V[i,n]*dt
             print(tab_euler_X)
     tab_euler_V = np.zeros(i*N)
+    
     #stockage des valeur de dV de A et B dans dV :
     dV= np.vstack((fA(V,dt),fB(V,dt)))
+    
     #stockage des valeurs de V pour A dans tab_euler_V :
     for i in range(0,i):
         tab_euler_V[i]=V[i,0]+dV[0,i]*dt
         print(tab_euler_V)
+        
     #stockage des valeurs de B dans tab_euler_V :
     for i in range(i,2*i):
         tab_euler_V[i]=V[i,1]+dV[1,i]*dt
