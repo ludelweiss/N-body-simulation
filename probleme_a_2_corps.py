5053ed9e2162b97f54a5e3dd209dd64caa6f11d0
SORCE Annaelle <s18017721@V-PJ-08-021.salsa.univ-amu.fr>
2022-02-24 10:30:02 +0100

    rectification euler

--- probleme_a_2_corps.py
+++ probleme_a_2_corps.py
@@ -11,24 +11,17 @@ G=1
 ma=1
 mb=1
 
+#nb d'objets :
 N=2
+#nb de coordonnées :
 i=3
 
 X=np.zeros((i,N))
-X[0,1]=1    #position initial de la particule B
+X[0,1]=1    #positions initiales des particules A et B
 
 V=np.zeros((i,N))
-V[0,1]=1    #vitesse initiale de la particule B
+V[0,1]=1    #vitesse initiale des particules A et B
 
-
-'''
-Norme des vitesses :
-va=np.sqrt(V[0,0]**2+V[1,0]**2+V[2,0]**2)
-vb=np.sqrt(V[0,1]**2+V[1,1]**2+V[2,1]**2)
-
-Energie cinétique :
-Ecb=0.5*mb*vb**2
-'''
 
 # Distance entre A et B :
 def rab(X):
@@ -65,27 +58,37 @@ def fB(V, t):
 
 
 def euler(dt, X, V, N, i):
-    tab_euler_X = np.zeros(i*N)
     
     #calcul du tableau des X pour A et B :
+    tab_euler_X = np.zeros(i*N)
     for n in range(0,N):
-        for i in range(0,i) :
-            tab_euler_X[i]=X[i,n]+V[i,n]*dt
-            print(tab_euler_X)
-    tab_euler_V = np.zeros(i*N)
+        for i in range(0,i*N) :
+            if (i<3):
+                tab_euler_X[i]=X[i,n]+V[i,n]*dt
+            if (i>=3):
+                tab_euler_X[i]=X[i-3,n]+V[i-3,n]*dt
+        i=3
+    tab_euler_X=tab_euler_X.reshape((3,N))
+    print("tab des positions :",tab_euler_X)
     
+    i=3
+    tab_euler_V = np.zeros(i*N)
     #stockage des valeur de dV de A et B dans dV :
     dV= np.vstack((fA(V,dt),fB(V,dt)))
+    print("dv=",dV)
     
+    i=3
     #stockage des valeurs de V pour A dans tab_euler_V :
     for i in range(0,i):
         tab_euler_V[i]=V[i,0]+dV[0,i]*dt
-        print(tab_euler_V)
-        
+    
+    i=3
     #stockage des valeurs de B dans tab_euler_V :
     for i in range(i,2*i):
-        tab_euler_V[i]=V[i,1]+dV[1,i]*dt
-        print(tab_euler_V)
+        tab_euler_V[i]=V[i-3,1]+dV[1,i-3]*dt
+    
+    tab_euler_V=tab_euler_V.reshape(3,N)
+    print("tab des vitesses :",tab_euler_V)
 
     return tab_euler_X , tab_euler_V
 
