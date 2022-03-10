2c79bc13c41a0b7cef4b667f86eba399b549da40
ALLARD Ludmilla <a19005800@V-PP-08-005.salsa.univ-amu.fr>
2022-03-10 11:58:37 +0100

    Rectifications dans méthode euler 2 part + début leapfrog

--- probleme_a_2_corps.py
+++ probleme_a_2_corps.py
@@ -32,8 +32,7 @@ V[0,1]=1    #vitesse initiale des particules A et B
 
 # Distance entre A et B :
 def rab(X):
-    rab=np.sqrt( (V[0,1]-V[0,0])**2 + (V[1,1]-V[1,0])**2 + (V[2,1]-V[2,0])**2 )
-    rab=np.sqrt( np.sum((V[:,0]-V[:,1])**2) )
+    rab=np.sqrt( np.sum((X[:,0]-X[:,1])**2) )
     return rab
     
 # Norme de la force entre A et B en fonction de rab :
@@ -63,6 +62,7 @@ def fB(V, t):
 '''
 Méthodes d'intégration
 '''
+
 # Méthode d'intégration Euler
 def euler(X, V, i, N, dt):
     tab_euler_X = X
@@ -70,31 +70,92 @@ def euler(X, V, i, N, dt):
     
     #calcul du tableau des X pour A et B :
     for n in range(0,N):
-        for i in range(0,i) :
-            tab_euler_X[i,n]=tab_euler_X[i,n]+tab_euler_V[i,n]*dt
+        for j in range(0,i) :
+            tab_euler_X[j,n]=tab_euler_X[j,n]+tab_euler_V[j,n]*dt
     
     #stockage des valeur de dV de A et B dans dV :
     dV= np.vstack((fA(V,dt),fB(V,dt))).T
     
-    i=3
+
     #stockage des valeurs de V pour A dans tab_euler_V :
-    for i in range(0,i):
-        tab_euler_V[i,0]=tab_euler_V[i,0]+dV[i,0]*dt
+    for j in range(0,i):
+        tab_euler_V[j,0]=tab_euler_V[j,0]+dV[j,0]*dt
     
-    i=3
+
     #stockage des valeurs de B dans tab_euler_V :
-    for i in range(0,i):
-        tab_euler_V[i,1]=tab_euler_V[i,1]+dV[i,1]*dt
+    for j in range(0,i):
+        tab_euler_V[j,1]=tab_euler_V[j,1]+dV[j,1]*dt
     
-    i=3
 
     return tab_euler_X , tab_euler_V
 
+
+"""
+# Distance entre A et B :
+def rab(X):
+    rab=np.sqrt( np.sum((V[:,0]-V[:,1])**2) )
+    return rab
+
+# Norme de la force entre A et B en fonction de rab :
+def Fab(rab):
+    Fab=G*ma*mb*(1/rab**2)
+    return Fab
+
+
+# Calcul du dv ajouté à chaque dt pour A :
+def fA(V, t):
+    dV=np.zeros(3)
+    dV[0]= Fab(rab(X))*t
+    dV[1]= 0
+    dV[2]= 0
+    return dV
+
+
+# Calcul du dv ajouté à chaque dt pour B :
+def fB(V, t):
+    dV=np.zeros(3)
+    dV[0]= -Fab(rab(X))*t
+    dV[1]= 0
+    dV[2]= 0
+    return dV
+"""
+
+def force(X, t, N) :
+    r = np.sqrt( np.sum((X[:,0]-X[:,1])**2) )   # distance entre les deux points
+    F = G*ma*mb/(r**2)  # force appliquée
+    dV = np.zeros(3)
+    dV[0]= F*t
+    dV[1]= 0
+    dV[2]= 0
+    return dV
 
 #Méthode d'intégration Leapfrog
 def leapfrog(X, V, i, N, dt) :
     tab_lf_X = X
     tab_lf_V = V
+    
+    for part in range(0,N) :
+        
+        dV= force(X, dt).T
+        
+        #calcul du tableau des X pour A et B :
+        for n in range(0,N) :
+            for j in range(0,i) :
+                v_1_2 = tab_lf_V[j,n] + 1/2*dt*dV[j,0]       #calcul de v_(n+1/2)
+                tab_lf_X[j,n] = tab_lf_X[j,n] + dt*v_1_2
+                    
+        
+        dV= np.vstack((fA(V,dt),fB(V,dt))).T
+        
+        #stockage des valeurs de V pour A dans tab_lf_V :
+        for j in range(0,i) :
+            tab_lf_V[j,0] = v_1_2 + 1/2*dt*dV[j,0]
+        
+        #stockage des valeurs de B dans tab_lf_V :
+        for j in range(0,i) :
+            tab_lf_V[j,1] = v_1_2 + 1/2*dt*dV[j,1]
+        
+    return tab_lf_X ,  tab_lf_V
   
 
 
@@ -115,4 +176,18 @@ def mouvement(X,V,i,N,dt,T):
         tab_euler=euler(X,V,i,N,dt)
         VAL_X[:,:,it]=tab_euler[0]
         VAL_V[:,:,it]=tab_euler[1]   
+    return VAL_X, VAL_V
+
+
+def mouvement2(X,V,i,N,dt,T):
+    NT = int(T/dt)
+    #tableau de taille (N,i,dt) pour stocker les valeurs finales de X et V :
+    VAL_X=np.zeros((i,N,NT))
+    VAL_X[:,:,0]=X
+    VAL_V=np.zeros((i,N,NT))
+    VAL_V[:,:,0]=V
+    for it in range(1, NT):
+        tab_lf=leapfrog(X,V,i,N,dt)
+        VAL_X[:,:,it]=tab_lf[0]
+        VAL_V[:,:,it]=tab_lf[1]   
     return VAL_X, VAL_V
